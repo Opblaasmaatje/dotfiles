8905938c1cbad4646caeabe28c4312287123a29d
Opblaasmaatje <joris.terwolbeek@outlook.com>
2025-05-02 17:53:43 +0200

    fix

--- init.sh
+++ init.sh
@@ -1,11 +1,2 @@
-echo "Want to enable preferred terminal settings?"
-read -k 1 "answer? Proceed(y/n)"
-
-if ! [[ "$answer" =~ [yY] ]]; then
-	exit 0;
-fi
-
-echo "Speeding up dock! ⏩"
-
 defaults write com.apple.dock autohide-time-modifier -float 0.15;
-killall Dock
+killall Dock;
